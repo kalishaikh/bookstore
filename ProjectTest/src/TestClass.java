8e82caa2101cf5f7cd9dcd8db1aa70874253a847
Kaasim Shaikh <53850980+kalishaikh@users.noreply.github.com>
2021-03-20 11:59:15 -0400

    Checking for Java Changes

--- ProjectTest/src/TestClass.java
+++ ProjectTest/src/TestClass.java
@@ -2,6 +2,11 @@
 public class TestClass {
 
 	/*
-	 * This is a test class 
+	 * This is a test class
 	 */
+	
+	public static void main (String [] args) {
+		
+		System.out.println("Printing a string");
+	}
 }
